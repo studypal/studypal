9ef91f85c11a70abfa5d0dbbe49464fa5c6713fd
sean1292 <sean.sadykoff@gmail.com>
2020-02-22 16:12:39 -0800

    pulled Parvanas code

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Error from './components/Error.jsx';
 
 class App extends React.Component {
   render() {
-    return <div>Hello, React!</div>;
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            {/*  need to put login component so that on / it would go to login*/}
+            {/* <Route path="/" component={} exact />
+      <Route path="/users" component={} />
+      <Route path="/msg" component={} />
+      <Route path="/register" component={} />*/}
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
