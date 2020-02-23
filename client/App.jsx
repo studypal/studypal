40eaf153ac28a03ea9194884fda1c7011d3a5b76
parvana07 <peroinitworld@gmail.com>
2020-02-22 19:39:29 -0800

    added some styling to login and register page and add functionality to login page-submit buttons gets value

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
-import Checkbox from "./components/Checkbox.jsx";
+
 // import Checkbox from
 import "./stylesheets/styles.css";
 
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,21 +1,61 @@
 import React, { Component } from "react";
 
-function Login(props) {
-  return (
-    <div className="login-register-input">
-      <h1>StudyPal</h1>
-      <p className="welcome">Welcome back!</p>
-      <form action="">
-        <input className="text" type="text" placeholder="Username" />
-        <input className="text" type="password" placeholder="Password" />
-        <input className="button" type="submit" value="Submit" />
-      </form>
-      <a href="">Forgot password?</a>
-      <br />
-      <span>New to StudyPal?</span>
-      <a href="">Join now</a>
-    </div>
-  );
+// function Login(props) {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    console.log(evt.target.name + " " + evt.target.value);
+    this.setState({
+      [evt.target.name]: evt.target.value
+
+      // [evt.target.name]: evt.target.value
+    });
+  }
+  handleSubmit(event) {
+    alert(
+      "A name and password was submitted: " +
+        this.state.username +
+        " " +
+        this.state.password
+    );
+  }
+  render() {
+    return (
+      <div className="login-register-input">
+        <h1>StudyPal</h1>
+        <p className="welcome">Welcome back!</p>
+        <form onSubmit={this.handleSubmit} action="">
+          <input
+            className="text"
+            type="text"
+            name="username"
+            onChange={this.handleChange}
+            placeholder="Username"
+          />
+          <input
+            className="text"
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            placeholder="Password"
+          />
+          <input className="button" type="submit" value="Submit" />
+        </form>
+        <a href="">Forgot password?</a>
+        <br />
+        <span>New to StudyPal?</span>
+        <a href="">Join now</a>
+      </div>
+    );
+  }
 }
 
 export default Login;
